8bba57e4bdc77ff7e7c685db680ce842a26ed11f
SleepedS1 <edwinsantiago3214@gmail.com>
2023-11-27 08:56:42 -0500

    ñ

--- server/routes/vuelos.js
+++ server/routes/vuelos.js
@@ -19,7 +19,23 @@ router.get('/consultar', (req, res) => {
 });
 
 router.post('/crear', (req, res) => {
-    res.status(404).json({ message: 'Ruta de crear vuelos.' });
+  // Aquí deberías obtener los datos del cuerpo de la solicitud (req.body)
+  // y luego realizar la lógica de inserción en la base de datos.
+  // Asumo que el cuerpo de la solicitud contiene los datos necesarios para crear un vuelo.
+
+  const { codvuelo, coddestino, codaerolinea, salaabordaje, horasalida, horallegada } = req.body;
+
+  // Asumiendo que tu tabla en la base de datos se llama "vuelo"
+  const query = 'INSERT INTO vuelo (codvuelo, coddestino, codaerolinea, salaabordaje, horasalida, horallegada) VALUES (?, ?, ?, ?, ?, ?)';
+
+  connection.query(query, [codvuelo, coddestino, codaerolinea, salaabordaje, horasalida, horallegada], (error, results) => {
+    if (error) {
+      console.error('Error al crear vuelo:', error);
+      res.status(400).json({ message: 'Error en la solicitud.' });
+    } else {
+      res.status(201).json({ message: 'Registro exitoso del vuelo.' });
+    }
+  });
 });
 
 module.exports = router;
