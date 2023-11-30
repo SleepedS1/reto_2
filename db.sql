acba5d2d5947ee0c9207f2098f926477a0b22a75
SleepedS1 <edwinsantiago3214@gmail.com>
2023-11-30 07:17:48 -0500

    s

--- db.sql
+++ db.sql
@@ -14,62 +14,13 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
--- Volcando estructura de base de datos para el_dorado_db
-CREATE DATABASE IF NOT EXISTS `el_dorado_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `el_dorado_db`;
-
--- Volcando estructura para tabla el_dorado_db.aerolinea
-CREATE TABLE IF NOT EXISTS `aerolinea` (
-  `codaerolinea` int DEFAULT NULL,
-  `descripcion` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- La exportación de datos fue deseleccionada.
 
 -- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla el_dorado_db.destino
-CREATE TABLE IF NOT EXISTS `destino` (
-  `coddestino` int DEFAULT NULL,
-  `descripcion` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 -- La exportación de datos fue deseleccionada.
 
--- Volcando estructura para tabla el_dorado_db.pasajero
-CREATE TABLE IF NOT EXISTS `pasajero` (
-  `id` int NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `apellidos` varchar(200) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `telefono` bigint NOT NULL,
-  `codvuelo` varchar(6) NOT NULL DEFAULT '0',
-  `foto` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 -- La exportación de datos fue deseleccionada.
-
--- Volcando estructura para tabla el_dorado_db.usuario
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` int NOT NULL,
-  `username` varchar(200) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `token` varchar(200) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- La exportación de datos fue deseleccionada.
-
--- Volcando estructura para tabla el_dorado_db.vuelo
-CREATE TABLE IF NOT EXISTS `vuelo` (
-  `codvuelo` varchar(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL DEFAULT '',
-  `coddestino` int NOT NULL,
-  `codaerolinea` int NOT NULL,
-  `salaabordaje` varchar(50) NOT NULL DEFAULT '0',
-  `horasalida` time NOT NULL,
-  `horallegada` time NOT NULL,
-  PRIMARY KEY (`codvuelo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- La exportación de datos fue deseleccionada.
 
