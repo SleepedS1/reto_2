5f645e571fde940125de4bd2fce80cbe23fc0619
SleepedS1 <edwinsantiago3214@gmail.com>
2023-11-30 11:33:24 -0500

    ss

--- admin/src/assets/GestionVuelos.jsx
+++ admin/src/assets/GestionVuelos.jsx
@@ -104,7 +104,7 @@ function GestionVuelos() {
           />
           <MyBoton
             text={'Salir'}
-            className={'flex bg-white px-2 py-1 rounded-xl shadow-lg text-black font-bold'}
+            className={'flex bg-red-400 px-2 py-1 rounded-xl shadow-lg text-black font-bold'}
             linkTo={'/'}
             children={
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
